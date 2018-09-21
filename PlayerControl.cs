fcbafbf07e069165b3c9a9df84b866671d6a14a1
Shycotic <42653218+Shycotic@users.noreply.github.com>
2018-09-21 16:26:15 -0400

    Add files via upload
    The code used for all three tiers in the roll a ball challenge. 
    As well as some code I tried to throw together such as code to move an obstacle and code to rotate that obstacle.

---
+++ PlayerControl.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+    public float speed;
+    public Text countText;
+    public Text pickupCountText;
+    public Text winText;
+    private int count;
+    private int pickupCount;
+    private Rigidbody rb;
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+    }
+    void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+        count = 0;
+        pickupCount = 0;
+        SetCountText();
+
+        winText.text = "";
+        
+    }
+
+        void FixedUpdate ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter (Collider other)
+    {
+      if (other.gameObject.CompareTag("Pick Up"))
+        {
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            pickupCount = pickupCount + 1;
+            //pickupCount++;
+            SetCountText ();
+        } 
+    
+      if (other.gameObject.CompareTag("RedPickUp"))
+        {
+            other.gameObject.SetActive(false);
+            count = count - 1;
+            //pickupCount++;
+            SetCountText();
+        }
+    }
+    void SetCountText()
+    {
+        countText.text = "Count: " + count.ToString();
+        pickupCountText.text = "Total: " + pickupCount.ToString();
+
+        if (pickupCount >= 12)
+        {
+            winText.text = "You Finished with score "+ countText.text;
+        }
+    }
+}
+//pickupCountText.text = "Total: " + pickupCount.ToString();
+//pickupCountText.text = "Total:";
